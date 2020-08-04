126690862b31534a53b96b53ffd64dbdfa3fe096
sambordo1 <sbordo@innovu.com>
2020-08-03 22:18:38 -0400

    working on logic

--- Ministries_Scheduler.py
+++ Ministries_Scheduler.py
@@ -1,32 +1,82 @@
-import pandas
-  
+import xlrd
+
 
 ushering = []
+ushersWeekly = []
+
 nursery = []
+nurseryWeekly = []
+
 security = []
+securityWeekly = []
+
 parking_patrol = []
-pham_driving= []
-sunday_school = []
-sound = []
-choir = []
-orchestra = []
-special_music = []
+parking_patrolWeekly =[]
+
+# pham_driving= []
+# sunday_school = []
+# sound = []
+# choir = []
+# orchestra = []
+# special_music = []
 
 
-excel_data_df = pandas.read_excel('Ministries.xlsx')
-excel_data_df = excel_data_df.fillna('')
+in_file = ("Ministries.xlsx")
+
+wb = xlrd.open_workbook(in_file)
+sheet = wb.sheet_by_index(0)
+
+data = [[sheet.cell_value(r, c) for r in range(sheet.nrows)] for c in range(sheet.ncols)]
+for arrays in data:
+	headings = arrays[0]
+	if headings == "Ushering":
+		ushering = arrays
+	if headings == "Nursery":
+		nursery = arrays
+	if headings == "Security":
+		security = arrays
+	if headings == "Parking Patrol":
+		parking_patrol = arrays
+
+	# if headings == "Pham Driving":
+	# 	pham_driving = arrays
+	# if headings == "Sunday School":
+	# 	sunday_school = arrays
+	# if headings == "Sound":
+	# 	sound = arrays
+	# if headings == "Choir":
+	# 	choir = arrays
+	# if headings == "Orchestra":
+	# 	orchestra = arrays
+	# if headings == "Special Music":
+	# 	special_music = arrays
+
+i = 1
+while i < 6:
+	ushersWeekly.append(ushering[i])
+	i += 1
+
+i = 1
+while i < 6:
+	nurseryWeekly.append(nursery[i])
+	i += 1
+
+i = 1
+while i < 6:
+	securityWeekly.append(security[i])
+	i += 1
+
+i = 1
+while i < 6:
+	parking_patrolWeekly.append(parking_patrol[i])
+	i += 1
+
+no_conflict = list(set(ushersWeekly) - set(nurseryWeekly))
+print(no_conflict)
+
+conflicts = list(set(ushersWeekly).intersection(set(nurseryWeekly)))
+print(conflicts)
 
 
-ushering = (excel_data_df['Ushering'].tolist())
-nursery = (excel_data_df['Nursery'].tolist())
 
 
-def create_schedule(usherList, nurseryList):
-	for each in usherList:
-		print(each)
-	for each in nurseryList:
-		print(each)
-
-
-
-create_schedule(ushering, nursery)
